6620f61b84be534cf04f001bbee5fadd04064836
Darkthunder119 <gurtajchhabra@gmail.com>
2020-04-28 14:09:38 -0400

    Added SideNav under Main.

--- tether/src/pages/MainPage/MainPage.jsx
+++ tether/src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import SideNav from '../../components/SideNav/SideNav'
 class MainPage extends React.Component{
 constructor(props){
     super(props);
 }
 
 render(){
-    return <>HIIII</>
+    return <>
+    <SideNav />
+    </>
 }
 }
 
