920534538ab2d3e7632187f1aed8ca9fbec71477
Darkthunder119 <gurtajchhabra@gmail.com>
2020-04-28 13:45:31 -0400

    Added GitHub and Google oauth and routing to /MainPage

--- tether/src/App.js
+++ tether/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage'
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          G TESTING STUFF
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter >
+        <Switch>
+          <Route exact path='/' component={MainPage} />
+          <Route path='/login' component={LoginPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
