5301dbfbec14e07fa93fa16e1f764b06a4cd3440
Darkthunder119 <gurtajchhabra@gmail.com>
2020-04-28 14:58:56 -0400

    Login Logout flow @John I detect 2 renders, no inf loop but something is off. take a look!

--- tether/src/pages/MainPage/MainPage.jsx
+++ tether/src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
-import SideNav from '../../components/SideNav/SideNav'
-class MainPage extends React.Component{
-constructor(props){
+import React from "react";
+import SideNav from "../../components/SideNav/SideNav";
+import * as firebase from "firebase";
+class MainPage extends React.Component {
+  constructor(props) {
     super(props);
-}
+    this.auth = firebase.auth();
+    this.state = {
+      user: "",
+    };
+  }
+  mounted = false;
 
-render(){
-    return <>
-    <SideNav />
-    </>
-}
+  authChange() {
+    this.auth.onAuthStateChanged((cred) => {
+      if (this.mounted) {
+        if (cred) {
+          this.setState({ user: cred.email }, () =>
+            console.log("statechanged", this.state.user)
+          );
+        } else {
+          this.props.history.push("/login");
+          console.log("signout detected since no login");
+        }
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.mounted=true;  
+    this.authChange();
+    console.log("didMount");
+  }
+  componentDidUpdate() {
+    if (!this.state.user && this.mounted) {
+      this.authChange();
+      console.log('hello');
+    }
+    console.log("didUpdate");
+  }
+
+  componentWillUnmount(){
+      this.mounted=false;
+  }
+
+  render() {
+    console.log(this.state.user);
+    return (
+      <>
+        <SideNav />
+      </>
+    );
+  }
 }
 
 export default MainPage;
