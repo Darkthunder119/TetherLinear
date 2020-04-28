6597efd2d9c2fa38efd4605541331ecb19b6606d
konnerb <konner.bouchard@hotmail.com>
2020-04-28 19:50:26 -0400

    Created Second Task Add Component, Updated Styling In Body Component, Added React-Switch

--- tether/src/components/TaskCard/TaskCard.jsx
+++ tether/src/components/TaskCard/TaskCard.jsx
@@ -1,25 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './TaskCard.scss'
+import Switch from "react-switch";
 
-export default function TaskCard({ ticket, title, description }) {
-    
+export default class TaskCard extends Component {
+    constructor() {
+        super();
+        this.state = { 
+            checked: false 
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+     
+        handleChange(checked) {
+        this.setState({ checked });
+        }
+
+        handleClick(checked) {
+        this.setState({ checked });
+        }
+
+    render() {
+        const active = this.props;
+        const { ticket, title, description } = this.props;
+
     return (
-        <div className="task">
-            <div className="task__section">
-                <span className="task__ticket">#WF-102{ticket}</span>
-                <button className="task__options">...</button>
-            </div>
+    <>
+        {this.props.description 
 
-            <div className="task__section">
-                <h4 className="task__title">UI For Components{title}</h4>
-                <p className="task__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur laudantium, dolorum sed velit cum rem aperiam deserunt ad id laborum neque nostrum iste sequi accusantium sapiente quibusdam eum eos incidunt.{description}</p>
-            </div>
+        ?   <div className="task">
+                <div className="task__section">
+                    <span className="task__ticket">{ticket}</span>
+                    <button className="task__options">...</button>
+                </div>
 
-            <div className="task__section">
-                <button className="task__toggle">toggle</button>
-                <button className="task__button">Mark as Complete</button>
-            </div>
-        </div>
+                <div className="task__section">
+                    <h4 className="task__title">{title}</h4>
+                    <p className="task__description">{description}</p>
+                </div>
 
-    )
+                <div className="task__section">
+                    <Switch 
+                        onChange={this.handleChange} 
+                        uncheckedIcon={false} 
+                        checked={this.state.checked}
+                        checkedIcon={false} 
+                    />
+                    <button className="task__button" onClick={this.onClick}>Mark as Complete</button>
+                </div>
+            </div>
+        
+        :   <div className="task">
+                <span className="task__add">+</span>
+                <h4 className="task__title">Add Next Task</h4>
+            </div>
+        }
+    </>
+    )}
 }
