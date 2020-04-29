88e7d47b9991772cd50a408defbbe069c69d2d0d
konnerb <konner.bouchard@hotmail.com>
2020-04-28 20:55:10 -0400

    Changed Color To Brainstation Blue, Changed div to seciton For TaskCard In Body Component

--- tether/src/components/Body/Body.jsx
+++ tether/src/components/Body/Body.jsx
@@ -7,14 +7,14 @@ export default class Body extends Component {
     return (
       <>
         <main className="body">
-          <div className="body__taskcards">
+          <section className="body__taskcards">
             <TaskCard 
             ticket="#WF-102" 
             title="UI For Components" 
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur laudantium, dolorum sed velit cum rem aperiam deserunt ad id laborum neque nostrum iste sequi accusantium sapiente quibusdam eum eos incidunt."
             />
             <TaskCard />
-          </div>
+          </section>
           <section className="body__tasks">ACTIVE TASKS SECTION</section>
           <section className="body__backlog">BACKLOG SECTION</section>
         </main>
