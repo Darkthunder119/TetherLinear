748b25fdac3a76c18aafa85fa6826eca81abe61b
Eula Bengco <eulabengco@gmail.com>
2020-04-29 01:32:19 -0400

    added undefined code to tasklist

--- tether/src/components/Body/Body.jsx
+++ tether/src/components/Body/Body.jsx
@@ -5,13 +5,17 @@ import TasksList from "../TasksList/TasksList";
 export default class Body extends Component {
   render() {
     console.log(this.props.jiraTasks);
+    let tasks = "";
+    if (this.props.jiraTasks !== undefined) {
+      tasks = <TasksList jiraTasks={this.props.jiraTasks} />;
+    } else {
+      tasks = <div>NO DATA, click BUTTON</div>;
+    }
     return (
       <>
         <main className="body">
           <section></section>
-          <section className="body__tasks">
-            <TasksList jiraTasks={this.props.jiraTasks} />{" "}
-          </section>
+          <section className="body__tasks">{tasks}</section>
           <button
             className="temporaryButton"
             onClick={this.props.populateJiraTasks}
--- tether/src/components/TasksList/TasksList.jsx
+++ tether/src/components/TasksList/TasksList.jsx
@@ -4,6 +4,7 @@ import "./TasksList.scss";
 
 export default class TasksList extends Component {
   // jiraTasks = this.props.jiraTasks;
+  /*
   tasks = this.props.jiraTasks.map((obj) => {
     const taskInfo = {
       id: obj.id,
@@ -16,21 +17,41 @@ export default class TasksList extends Component {
     };
     return taskInfo;
   });
+  */
   render() {
     console.log(this.props.jiraTasks);
+    // if (this.props.jiraTasks !== undefined) {
+    // }
 
     //   console.log(this.props.jiraTasks);
     //   this.props.jiraTasks.map((obj) => <TaskCard {...obj} key={obj.id} />);
     //if (this.props.jiraTasks)
-    return (
-      <>
-        <h1>My Tasks ({this.tasks.length})</h1>
+    if (this.props.jiraTasks !== undefined) {
+      let tasks =
+        this.props.jiraTasks.length > 0
+          ? this.props.jiraTasks.map((obj) => {
+              const taskInfo = {
+                id: obj.id,
+                name: obj.value.name,
+                priority: obj.value.priority,
+                isCompleted: obj.value.isCompleted,
+                isInProgress: obj.value.isInProgress,
+                number: obj.value.ticketNumber,
+                details: obj.value.details,
+              };
+              return taskInfo;
+            })
+          : null;
+    }
+    return <></>;
+  }
+}
+
+/*
+ <h1>My Tasks ({this.tasks.length})</h1>
         <div className="tasks__container">
           {this.tasks.map((obj) => (
             <TaskCard {...obj} key={obj.id} />
           ))}
         </div>
-      </>
-    );
-  }
-}
+        */
