f58f4e1ecece65964bce29632892e24aa9e18283
Tiffany Chan <tiffanyvchan@gmail.com>
2020-04-28 21:15:05 -0400

    fixed styling on creatmodal for personal goals

--- tether/src/components/CreateModal/CreateModal.jsx
+++ tether/src/components/CreateModal/CreateModal.jsx
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import "./CreateModal.scss";
 import Modal from "react-modal";
 
-// Modal.setAppElement("#root");
-
 const modalStyles = {
   content: {
     position: "absolute",
@@ -20,28 +18,85 @@ const modalStyles = {
   }
 };
 
-const CreateModal = ({ isOpen, closeModal }) => {
+class CreateModal extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      goals:[""]
+    };
+  }
 
-  // handleSubmit function
+  addGoal(e){
+    e.preventDefault();
+    this.setState({goals: [...this.state.goals, " "]})
+  }
 
+  handleChange(e, index){
+    e.preventDefault();
+    this.state.goals[index] = e.target.value
+    // set the changed state
+    this.setState({goals: this.state.goals})
+  }
+
+  handleRemove(e,index){
+    e.preventDefault();
+    // remove an item at the index
+    this.state.goals.splice(index,1)
+
+    console.log("thingy", this.state.goals);
+
+    // update the state
+    this.setState({goals: this.state.goals})
+  }
+
+  handleSubmit
+
+  render() {
   return (
     <>
-    {isOpen && (
-      <Modal isOpen={isOpen} style={modalStyles}>
+    {this.props.isOpen && (
+      <Modal isOpen={this.props.isOpen} style={modalStyles}>
          <form onSubmit="handleSubmit" className="modal-form">
            <div className="modal-form__top">
               <h2 className="modal-form__heading">Personal Goals</h2>
               <p>Create Your Weekly Personal Goals. </p>
-              <div className="modal-form__row">
-                <div className="modal-form__field">
-                  <label className="modal-form__label"></label>
-                  <input
-                    className="modal-form__input"
-                    name="name"
-                    placeholder="Do 15 minutes of Yoga Everyday"
-                    />
-                </div>
+              <div className="modal-form__subheading-wrap">
+                <h3 className="modal-form__subheading">Goal</h3>
+                <h3 className="modal-form__subheading modal-form__subheading--second"># Times/Week</h3>
               </div>
+
+                  {
+                    this.state.goals.map((goal, index) => {
+                      return(
+                        <div className="modal-form__row">
+                          <div className="modal-form__field">
+                            <label className="modal-form__label"></label>
+                            <input
+                            onChange={(e) => this.handleChange(e)}
+                            className="modal-form__input"
+                            name="name"
+                            key={index}
+                
+                            placeholder="Ex.: Do 15 minutes of Yoga Everyday"
+                            />
+                          </div>
+                          <div className="modal-form__field">
+                            <label className="modal-form__label"></label>
+                            <select className="modal-form__input modal-form__input--small" name="times">
+                              <option value="1">1</option>
+                              <option value="2">2</option>
+                              <option value="3">3</option>
+                              <option value="4">4</option>
+                              <option value="5">5</option>
+                            </select>
+                          </div>
+                          <button className="modal-form__remove-btn" onClick={(e) => this.handleRemove(e)}>remove</button>
+                        </div>
+                        )
+                    })
+                  }
+  
+                <button className="modal-form__add-btn" onClick={(e)=>this.addGoal(e)}>+</button>
             </div>
             
             <div className="modal-form__buttonwrap">
@@ -51,7 +106,7 @@ const CreateModal = ({ isOpen, closeModal }) => {
               >
                 SAVE
               </button>
-              <button className="modal-form__button" onClick={closeModal}>
+              <button className="modal-form__button" onClick={this.props.closeModal}>
                 CANCEL
               </button>
             </div>
@@ -60,6 +115,7 @@ const CreateModal = ({ isOpen, closeModal }) => {
     )}
     </>
   )
+    }
 }
 
 export default CreateModal;
