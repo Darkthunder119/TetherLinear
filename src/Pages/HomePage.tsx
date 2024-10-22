90b3e92e24ff8850aa9e8d629cea407acd74e1c1
Gurtaj Chhabra <gurtajsingh@live.com>
2024-10-22 14:27:01 -0400

    UI updates

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const HomePage = () => (
     <div className="bg-primaryPetrol w-screen h-screen">
         <div className="flex w-full h-full gap-2">
-            <div className="text-offWhite text-xl w-1/5 flex items-center flex-col border-r-4 border-secondaryWheat-500">
-                <div>Boilerplate updated for Oct 2024</div>
-                <div>Testing left hand pane</div>
+            <div className="text-offWhite text-xl w-1/12 flex flex-col border-r-4 border-secondaryWheat-500 px-4 justify-between">
+                <div className="py-4">
+                    <div>Home</div>
+                    <div>Page1</div>
+                </div>
+                <div className="py-4 underline">
+                    <Link to="/logout">Log Out</Link>
+                </div>
             </div>
             <Outlet />
         </div>
