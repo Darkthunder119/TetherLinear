4f4170e09471f8cde62c28317b00aad2da136190
Gurtaj Chhabra <45543447+Darkthunder119@users.noreply.github.com>
2024-10-22 06:24:58 -0400

    [v0.2] Fix build failure (#58)
    * TET-17 TET-21 (#45)
    
    * added urql and react-router
    
    * added some basic Error and Routing
    
    * added updated packages for urql,reactrouter, graphql
    
    * removed env example file. use env from github now
    
    * moved to constants file.
    
    * fixed routing table and added proper ErrorBoundaries
    
    * fixed build failure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider, Outlet, useRouteError } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Dashboard from './Pages/Dashboard';
 import Error from './Pages/Error';
--- src/Pages/Error.tsx
+++ src/Pages/Error.tsx
@@ -3,8 +3,6 @@ import { Link, useRouteError, ErrorResponse } from 'react-router-dom';
 const Error = () => {
     const error = useRouteError() as ErrorResponse;
 
-    console.error(error);
-
     return (
         <div className="bg-primaryPetrol w-screen h-screen text-offWhite text-xl flex justify-center items-center flex-col">
             <div>{error.statusText || 'Oops! An unexpected error occurred'}</div>
