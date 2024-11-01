6fc1efbdacc4f7594a948fb0cc44cf8a54a883e3
Gurtaj Chhabra <gurtajsingh@live.com>
2024-11-01 06:35:25 -0400

    added some identity management

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,15 +5,14 @@ import { Link, Outlet } from 'react-router-dom';
 import { useAuth } from '@/lib/helpers/useAuth';
 
 const Navbar: FC = () => {
-    const { signOut } = useAuth();
+    const { signOut, currentUser } = useAuth();
 
     return (
         <div className="bg-shadesBlack w-screen h-screen">
             <div className="flex w-full h-full gap-2">
                 <div className="text-offWhite text-xl w-1/12 flex flex-col border-r-4 border-secondaryWheat-500 px-4 justify-between">
                     <div className="py-4">
-                        <div>Home</div>
-                        <div>Page1</div>
+                        <div>Hi {currentUser?.displayName}</div>
                     </div>
                     <div className="py-4 underline">
                         <Link
