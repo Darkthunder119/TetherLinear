4fe25add7caf4ddeb4624b674a2cdd1cbca7ef54
Gurtaj Chhabra <gurtajsingh@live.com>
2024-11-01 06:54:10 -0400

    added more components from shadcn and setup avatar for usage

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,15 +4,21 @@ import { Link, Outlet } from 'react-router-dom';
 
 import { useAuth } from '@/lib/helpers/useAuth';
 
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+
 const Navbar: FC = () => {
     const { signOut, currentUser } = useAuth();
 
     return (
-        <div className="bg-shadesBlack w-screen h-screen">
+        <div className="bg-shadesOffWhite w-screen h-screen">
             <div className="flex w-full h-full gap-2">
-                <div className="text-offWhite text-xl w-1/12 flex flex-col border-r-4 border-secondaryWheat-500 px-4 justify-between">
-                    <div className="py-4">
-                        <div>Hi {currentUser?.displayName}</div>
+                <div className="text-shadesBlack text-xl w-1/12 flex flex-col border-r-4 border-shadesBlack px-4 justify-between">
+                    <div className="py-4 flex gap-4 items-center">
+                        <Avatar className="cursor-pointer">
+                            <AvatarImage src={currentUser?.photoURL || undefined} />
+                            <AvatarFallback>{currentUser?.displayName?.charAt(0)}</AvatarFallback>
+                        </Avatar>
+                        <div>{currentUser?.displayName}</div>
                     </div>
                     <div className="py-4 underline">
                         <Link
--- src/lib/context/AuthContext.tsx
+++ src/lib/context/AuthContext.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, createContext, FC, ReactNode } from 'react';
 
 import { User, GoogleAuthProvider, GithubAuthProvider, signInWithPopup, Auth } from 'firebase/auth';
 
+import SkeletonCard from '@/components/Skeleton/SkeletonCard';
 import { useToast } from '@/hooks/use-toast';
 
 import auth from '../firebase';
@@ -65,7 +66,13 @@ export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
 
     return (
         <AuthContext.Provider value={{ currentUser, signIn, signOut }}>
-            {loading ? <>Loading...</> : children}
+            {loading ? (
+                <div className="w-screen h-screen flex items-center justify-center">
+                    <SkeletonCard />
+                </div>
+            ) : (
+                children
+            )}
         </AuthContext.Provider>
     );
 };
